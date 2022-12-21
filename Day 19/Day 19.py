fb0461361ef1b071949a32bc18a5a6b194a16f71
BowTieBird <conacherjohn@gmail.com>
2022-12-21 00:05:36 +0000

    Day 19 WIP

--- Day 19/Day 19.py
+++ Day 19/Day 19.py
@@ -5,57 +5,61 @@ file = open("input.txt")
 p = re.compile("Blueprint (.*): Each ore robot costs (.*) ore. Each clay robot costs (.*) ore. Each obsidian robot costs (.*) ore and (.*) clay. Each geode robot costs (.*) ore and (.*) obsidian.")
 blueprints_re = [ p.search(line) for line in file.readlines() ]
 
-def maximiseGeodes(blue_print, robots, resources, t):
+def maximiseGeodes(blueprint, robots, resources, t, next_buy=None, collect_resources=False):
     if t == 0:
         return resources['geode']
 
-    ore_robot_cost = int(blue_print.group(2))
-    clay_robot_cost = int(blue_print.group(3))
-    obsidian_robot_ore_cost = int(blue_print.group(4))
-    obsidian_robot_clay_cost = int(blue_print.group(5))
-    geode_robot_ore_cost = int(blue_print.group(6))
-    geode_robot_obsidian_cost = int(blue_print.group(7))
+    ore_robot_cost = int(blueprint.group(2))
+    clay_robot_cost = int(blueprint.group(3))
+    obsidian_robot_ore_cost = int(blueprint.group(4))
+    obsidian_robot_clay_cost = int(blueprint.group(5))
+    geode_robot_ore_cost = int(blueprint.group(6))
+    geode_robot_obsidian_cost = int(blueprint.group(7))
     costs = {'ore':{'ore':ore_robot_cost}, 'clay':{'ore':clay_robot_cost}, 'obsidian':{'ore':obsidian_robot_ore_cost, 'clay':obsidian_robot_clay_cost}, 'geode':{'ore':geode_robot_ore_cost, 'obsidian':geode_robot_obsidian_cost}}
 
-    # Only buy when "just reached"
-    possible_buys = [{}]
+    # Collection (We'll say it happens at the beginning of the following minute)
+    if collect_resources:
+        for resource in resources.keys():
+            resources[resource] += robots[resource]
 
-    if resources['ore'] == ore_robot_cost: 
-        possible_buys.append({'ore':1})
-
-    if resources['ore'] == clay_robot_cost:
-        possible_buys.append({'clay':1})
-
-    if (resources['ore'] == obsidian_robot_ore_cost and resources['clay'] >= obsidian_robot_clay_cost) \
-            or (resources['ore'] >= obsidian_robot_ore_cost and resources['clay'] == obsidian_robot_clay_cost):
-        possible_buys.append({'obsidian':1})
-
-    if (resources['ore'] == geode_robot_ore_cost and resources['obsidian'] >= geode_robot_obsidian_cost) \
-            or (resources['ore'] >= geode_robot_ore_cost and resources['obsidian'] == geode_robot_obsidian_cost):
-        possible_buys.append({'geode':1})
-    
-    # Collection
-    for resource in resources.keys():
-        resources[resource] += robots[resource]
-    print(possible_buys)
-    print(resources)
+    if next_buy is None:
+        # Possible choices for the next buy
+        next_possible_buys = {'ore', 'clay'}
+        if robots['clay'] >= 1:
+            next_possible_buys.add('obsidian')
+        if robots['obsidian'] >= 1:
+            next_possible_buys.add('geode')
+    else:
+        next_possible_buys = { next_buy }
+        
+    # Determine which can be bought immediately
+    possible_immediate_buys = set()
+    for buy in next_possible_buys:
+        cost = costs[buy]
+        buyable = True
+        for resource in cost.keys():
+            if cost[resource] > resources[resource]:
+                buyable = False
+                break
+        # if all([cost[resource] >= resources[resource] for resource in cost.keys()]):
+        if buyable:
+            possible_immediate_buys.add(buy)      
 
     # Return maximum buy
-    possible_cracks = []
-    for buy in possible_buys:
-        new_robots = { robot: val + buy[robot] if robot in buy.keys() else val for robot, val in robots.items() }
-        new_resources = { resource: val - sum([buy[robot] * costs[robot][resource] for robot in robots.items() if robot in buy.keys() and resource in costs[robot].keys()]) for resource, val in resources.items() }
-        print([buy[robot] * costs[robot][resource] for robot in robots.items() if robot in buy.keys() and resource in costs[robot].keys() for resource in resources.keys()])
-        possible_cracks.append(maximiseGeodes(blue_print, new_robots,  new_resources, t-1))
+    possible_cracks = set()
+    for buy in next_possible_buys:
+        if buy in possible_immediate_buys:
+            new_robots =  { robot: val + 1 if robot == buy else val for robot, val in robots.items() }
+            new_resources = { resource: val - costs[buy][resource] if resource in costs[buy].keys() else val for resource, val in resources.items() }
+            possible_cracks.add(maximiseGeodes(blueprint, new_robots,  new_resources, t, collect_resources=False))
+        else:
+            possible_cracks.add(maximiseGeodes(blueprint, robots, resources, t-1, next_buy=buy, collect_resources=True))
     return max(possible_cracks)
 
-
-
-
-for blue_print in blueprints_re:
+for blueprint in blueprints_re:
     robots = { 'ore':1, 'clay':0, 'obsidian':0, 'geode':0 }
     resources =  { 'ore':0, 'clay':0, 'obsidian':0, 'geode':0 }
-    print(maximiseGeodes(blue_print, robots, resources, 24))
+    print(blueprint.group(1) * maximiseGeodes(blueprint, robots, resources, 10))
 
 
 
